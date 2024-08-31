6164593796b6f2726bbb257d9ead4bb008e505ed
sirzes02 <sirzes02@gmail.com>
2024-08-30 21:27:35 -0500

    add library basic view

---
+++ spotify-sub/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryAlbumsViewController.swift
+//  spotify-sub
+//
+//  Created by Santiago Varela on 30/08/24.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemOrange
+    }
+}
---
+++ spotify-sub/Controllers/Other/Library/LibraryPlaylistViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LibraryPlaylistViewController.swift
+//  spotify-sub
+//
+//  Created by Santiago Varela on 30/08/24.
+//
+
+import UIKit
+
+class LibraryPlaylistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
